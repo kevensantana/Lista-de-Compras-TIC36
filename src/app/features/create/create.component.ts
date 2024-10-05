703dddd384259c0a2d62ee12567f3284ee61776c
kevensantana <kevensantana40@gmail.com>
2024-10-05 01:58:29 -0300

    feat:adiciona funcionalidade de salvar

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { ProductsService } from './../../shared/services/products.service';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-create',
@@ -12,6 +14,9 @@ import { MatInputModule} from '@angular/material/input';
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  productsService = inject(ProductsService)
+
   form = new FormGroup({
     title: new FormControl<string>(' ', {
       nonNullable: true,
@@ -21,6 +26,12 @@ export class CreateComponent {
   });
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.productsService.post({
+      title: this.form.controls.title.value
+
+    })
+    .subscribe(() => {
+      alert('Sucesso')
+    })
   }
 }
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../Interfaces/product.interface';
+import { ProductPayload } from '../Interfaces/payload-product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,11 @@ export class ProductsService {
 
   HttpClient = inject(HttpClient);
 
-
   getAll() {
    return this.HttpClient.get<Product[]>('/api/products')
+  }
+
+  post(payload: ProductPayload) {
+   return this.HttpClient.post('/api/products', payload)
   }
 }
